ad4c4af6923f512fad148d95da66d199b26803b2
Dairon Leyva <dleyva@peopleticker.com>
2019-12-18 12:48:46 -0500

    added logging to cypress test

--- cypress/integration/wait/wait.spect.js
+++ cypress/integration/wait/wait.spect.js
@@ -1,16 +1,35 @@
 /// <reference types="Cypress" />
+
+const getQueryName = function(xhr) {
+  const regexp = /\w+Query/gm;
+  const match = xhr.requestBody.query.match(regexp);
+  return match[0];
+};
+
 context("Wait issue", () => {
   beforeEach(() => {
     cy.server();
-    cy.route({ method: "POST", url: "**/next-js-with-relay-modern-example" }).as("query");
+    cy.route({
+      method: "POST",
+      url: "**/next-js-with-relay-modern-example",
+      // optional route config added only for debugging
+      onRequest: xhr => console.log("onRequest:", getQueryName(xhr)),
+      onResponse: xhr => console.log("onResponse:", getQueryName(xhr)),
+      onAbort: args => console.log("onAbort:", args)
+    }).as("query");
   });
 
   it("does not timeout 01", () => {
+    // first we vist url which triggers
+    // waterfall calls to graphql endpoint
     cy.visit("/");
-    cy.wait("@query");
-    cy.wait("@query");
-    cy.wait("@query");
-    cy.wait("@query");
-    cy.wait("@query");
+
+    // now we wait for requests to resolve
+    // NOTE: .then(...) added only for debugging
+    cy.wait("@query").then(xhr => cy.log("query1:", getQueryName(xhr)));
+    cy.wait("@query").then(xhr => cy.log("query2:", getQueryName(xhr)));
+    cy.wait("@query").then(xhr => cy.log("query3:", getQueryName(xhr)));
+    cy.wait("@query").then(xhr => cy.log("query4:", getQueryName(xhr)));
+    cy.wait("@query").then(xhr => cy.log("query5:", getQueryName(xhr)));
   });
 });
